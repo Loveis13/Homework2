cccdc41edd19cf7d8970d7df80ba3b8e27400613
vlad <79054506676@mail.ru>
2023-05-07 13:49:51 +0300

    Homework2

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    public static void main(String[] args) {
+      var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var boxOne = 78.2;
+        System.out.println(boxOne);
+        var boxTwo = 82.7;
+        System.out.println(boxTwo);
+        var totalWeight = boxOne + boxTwo;
+        System.out.println("общий вес " + totalWeight);
+        var weightDifference = boxOne - boxTwo;
+        System.out.println ("разница веса бойцов " + weightDifference + "кг!");
+        var weidhtDifference = boxTwo % boxOne;
+        System.out.println("разница веса бойцов " + weidhtDifference);
+
+        var staff = 640;
+        staff = staff / 8;
+        System.out.println ("всего работников в компании " + staff );
+        var allWorkers = 80;
+        allWorkers = allWorkers + 94;
+        System.out.println (allWorkers);
+        var newWorkHours = allWorkers;
+        System.out.println(allWorkers);
+        var newWohourse = allWorkers * staff;
+        System.out.println(newWohourse);
+        System.out.println("Если в компании работает " + allWorkers + "то проделанно часов будет" + newWohourse + "часов");
+
+
+
+
+
+    }
+
+    }
+
